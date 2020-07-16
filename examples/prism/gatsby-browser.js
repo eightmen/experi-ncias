cae77a7c1517a7c6f610cb5e51c006c5c509a140
eightmen <118704527+eightmen@users.noreply.github.com>
2020-07-16 09:18:57 +0000

    things

---
+++ examples/prism/gatsby-browser.js
@@ -0,0 +1,7 @@
+
+import * as React from 'react'
+import { WrapRootElement } from './src'
+
+export const wrapRootElement = ({ element }) => (
+  <WrapRootElement element={element} />
+)
---
+++ examples/typography/gatsby-config.js
@@ -0,0 +1,24 @@
+
+// @ts-check
+
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  jsxRuntime: 'automatic',
+  jsxImportSource: 'theme-ui',
+  plugins: [
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: ['Work+Sans', 'Quattrocento+Sans:400,400i,700'],
+      },
+    },
+  ],
+}
