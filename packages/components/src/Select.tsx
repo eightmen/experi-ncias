37da87b9626ef54a117fc272127b5cc560ca9b31
eightmen <118704527+eightmen@users.noreply.github.com>
2020-09-16 14:57:34 +0000

    Update chapter2.md

--- packages/components/src/Select.tsx
+++ packages/components/src/Select.tsx
@@ -21,4 +21,47 @@ export interface SelectProps
 /**
  * Select variants can be defined in `theme.forms`
  * and the component uses the `theme.forms.select` variant by default.
- * @see https://theme-ui.com/components/s
+ * @see https://theme-ui.com/components/select/
+ */
+export const Select: ForwardRef<HTMLSelectElement, SelectProps> =
+  React.forwardRef(function Select({ arrow, ...props }, ref) {
+    const __css: ThemeUICSSObject = {
+      display: 'block',
+      width: '100%',
+      p: 2,
+      paddingRight: 4,
+      appearance: 'none',
+      fontSize: 'inherit',
+      lineHeight: 'inherit',
+      border: '1px solid',
+      borderRadius: 4,
+      color: 'inherit',
+      backgroundColor: (theme) => get(theme, 'colors.background', null),
+    }
+
+    return (
+      <Box
+        {...getMargin(props)}
+        sx={{
+          display: 'flex',
+        }}
+      >
+        <Box
+          ref={ref}
+          as="select"
+          variant="select"
+          {...(omitMargin(props) as BoxProps)}
+          {...__internalProps({ __themeKey: 'forms', __css })}
+        />
+        {arrow || (
+          <DownArrow
+            sx={{
+              ml: -28,
+              alignSelf: 'center',
+              pointerEvents: 'none',
+            }}
+          />
+        )}
+      </Box>
+    )
+  })
