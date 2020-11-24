51bfe839dee88215a22e9266c5806405a4d2b05b
eightmen <118704527+eightmen@users.noreply.github.com>
2020-11-24 14:44:07 +0000

    Update api.py

--- packages/core/test/index.tsx
+++ packages/core/test/index.tsx
@@ -118,4 +118,102 @@ describe('ThemeProvider', () => {
         { theme },
         jsx('div', {
           sx: {
-            variant: 'cards.d
+            variant: 'cards.default',
+          },
+        })
+      )
+    )
+    expect(json).toHaveStyleRule('border', '1px solid tomato')
+  })
+})
+
+describe('jsx', () => {
+  test('custom pragma adds styles', () => {
+    const json = renderJSON(
+      jsx('div', {
+        sx: {
+          mx: 'auto',
+          p: 2,
+          bg: 'tomato',
+        },
+      })
+    )
+    expect(json).toHaveStyleRule('margin-left', 'auto')
+    expect(json).toHaveStyleRule('margin-right', 'auto')
+    expect(json).toHaveStyleRule('padding', '8px')
+    expect(json).toHaveStyleRule('background-color', 'tomato')
+  })
+
+  test('adds raw values with css prop', () => {
+    const json = renderJSON(
+      jsx('div', {
+        css: {
+          margin: 4,
+        },
+      })
+    )
+    expect(json).toHaveStyleRule('margin', '4px')
+  })
+
+  test('css prop accepts functions', () => {
+    const json = renderJSON(
+      jsx(
+        ThemeProvider,
+        {
+          theme: {
+            colors: {
+              primary: 'tomato',
+            },
+          },
+        },
+        jsx('div', {
+          css: (t: Theme) => ({
+            color: t.colors?.primary,
+          }),
+        })
+      )
+    )
+    expect(json).toHaveStyleRule('color', 'tomato')
+  })
+
+  test('sx and css prop can be used together', () => {
+    const json = renderJSON(
+      jsx('div', {
+        css: {
+          margin: 0,
+        },
+        sx: {
+          bg: 'tomato',
+        },
+      })
+    )
+    expect(json).toHaveStyleRule('background-color', 'tomato')
+    expect(json).toHaveStyleRule('margin', '0')
+  })
+
+  test('custom pragma handles null props', () => {
+    const json = renderJSON(jsx('h1', null, 'hello'))
+    expect(json).toMatchSnapshot()
+  })
+
+  test('sx prop supports dot notation', () => {
+    const json = renderJSON(
+      jsx(
+        ThemeProvider,
+        {
+          theme: {
+            config: {
+              useCustomProperties: false,
+            },
+            colors: {
+              text: 'black',
+              base: {
+                blue: ['#07c'],
+                primary: 'cyan',
+              },
+            },
+          },
+        },
+        jsx('div', {
+          sx: {
+            color: 'ba
