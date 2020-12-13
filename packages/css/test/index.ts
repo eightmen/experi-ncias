359f0277262fff58cf9439ff55bbfd02922db413
eightmen <118704527+eightmen@users.noreply.github.com>
2020-12-13 22:44:13 +0000

    Update videos.js

--- packages/css/test/index.ts
+++ packages/css/test/index.ts
@@ -41,3 +41,123 @@ const theme: Theme = {
       fontWeight: 'bold',
       color: 'secondary',
       bg: 'background',
+    },
+    primary: {
+      p: 3,
+      fontWeight: 'bold',
+      color: 'white',
+      bg: 'primary',
+      borderRadius: 2,
+    },
+    size: {
+      size: '100%',
+      bg: 'primary',
+    },
+    round: {
+      variant: 'buttons.size',
+      overflow: 'hidden',
+      borderRadius: '50%',
+    },
+  },
+  text: {
+    caps: {
+      fontSize: [1, 2],
+      letterSpacing: '0.1em',
+      textTransform: 'uppercase',
+    },
+    title: {
+      fontSize: [3, 4],
+      letterSpacing: ['-0.01em', '-0.02em'],
+    },
+  },
+  borders: {
+    body: '3px solid #000000',
+  },
+  borderWidths: {
+    thin: 1,
+  },
+  borderStyles: {
+    thick: 'solid',
+  },
+  radii: {
+    small: 5,
+  },
+  opacities: [0, '50%'],
+  transitions: {
+    standard: '0.3s ease-in-out',
+  },
+  shadows: {
+    card: '5px 5px 15px 5px #000000',
+  },
+  zIndices: {
+    below: -1,
+    body: 1,
+    nav: 2,
+  },
+}
+
+test('returns a function', () => {
+  const result = css()
+  expect(typeof result).toBe('function')
+})
+
+test('returns an object', () => {
+  const result = css()()
+  expect(typeof result).toBe('object')
+})
+
+test('returns styles', () => {
+  const result = css({
+    fontSize: 32,
+    color: 'blue',
+    borderRadius: 4,
+  })()
+  expect(result).toEqual({
+    fontSize: 32,
+    color: 'blue',
+    borderRadius: 4,
+  })
+})
+
+test('returns system props styles', () => {
+  const result = css({
+    color: 'primary',
+    fontSize: [2, 3, 4],
+  })({ theme })
+  expect(result).toEqual({
+    fontSize: 16,
+    '@media screen and (min-width: 40em)': {
+      fontSize: 24,
+    },
+    '@media screen and (min-width: 52em)': {
+      fontSize: 36,
+    },
+    color: 'tomato',
+  })
+})
+
+test('returns nested system props styles', () => {
+  const result = css({
+    color: 'primary',
+    '&:hover': {
+      color: 'secondary',
+    },
+  })({ theme })
+  expect(result).toEqual({
+    color: 'tomato',
+    '&:hover': {
+      color: 'cyan',
+    },
+  })
+})
+
+test('returns nested responsive styles', () => {
+  const result = css({
+    color: 'primary',
+    h1: {
+      py: [3, 4],
+      scrollPaddingY: [2, 4],
+    },
+  })({ theme })
+  expect(result).toEqual({
+    
