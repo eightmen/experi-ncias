3394ba0e2ed278c7122147f0b304f26757a426e3
eightmen <118704527+eightmen@users.noreply.github.com>
2021-01-22 19:00:00 +0000

    huge refactor

--- packages/custom-properties/src/index.ts
+++ packages/custom-properties/src/index.ts
@@ -28,3 +28,25 @@ export default function makeCustomProperties(theme: Theme, prefix?: string) {
 
       if (prefix && !previousKey) {
         formattedKey = `${prefix}-${formattedKey}`
+      }
+
+      const newKey = previousKey
+        ? previousKey + '-' + formattedKey
+        : formattedKey
+
+      if (Array.isArray(value)) {
+        value.forEach((item, i) => {
+          customProperties[`--${newKey}-${i}`] = item
+        })
+      } else if (Object(value) === value) {
+        generateProperties(value, newKey)
+      } else {
+        customProperties[`--${newKey}`] = value
+      }
+    })
+  }
+
+  generateProperties(theme)
+
+  return customProperties
+}
