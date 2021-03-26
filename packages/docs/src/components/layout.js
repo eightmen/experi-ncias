183a0af9400bbf6c940687ab24667f2008a904d1
eightmen <118704527+eightmen@users.noreply.github.com>
2021-03-26 09:46:23 +0000

    Add installation instructions

--- packages/docs/src/components/layout.js
+++ packages/docs/src/components/layout.js
@@ -42,4 +42,75 @@ const getModeName = (mode) => {
 }
 
 export default function DocsLayout(props) {
-  const [menuOpe
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [mode, setMode] = useColorMode()
+
+  const { pathname } = props.location
+  const isLanding = pathname === '/'
+
+  const fullwidth =
+    isLanding ||
+    (props.pageContext.frontmatter && props.pageContext.frontmatter.fullwidth)
+
+  const showNav = !props.pageContext?.frontmatter?.hidenav
+
+  const nextColorMode = modes[(modes.indexOf(mode) + 1) % modes.length]
+
+  return (
+    <Fragment>
+      <Head {...props} />
+      <SkipLink>Skip to content</SkipLink>
+      <Flex
+        sx={{
+          flexDirection: 'column',
+          minHeight: '100vh',
+        }}
+      >
+        {showNav && (
+          <Flex
+            as="header"
+            sx={{
+              zIndex: 1,
+              height: 64,
+              px: 3,
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              position: isLanding ? 'initial' : 'sticky',
+              top: 0,
+              background: 'background',
+            }}
+          >
+            <Flex sx={{ alignItems: 'center' }}>
+              <MenuButton
+                onClick={(e) => {
+                  setMenuOpen(!menuOpen)
+                }}
+              />
+              <Link to="/" sx={{ variant: 'links.nav' }}>
+                Theme UI
+              </Link>
+            </Flex>
+            <Suspense>
+              <Flex sx={{ gap: [0, 2] }}>
+                <SearchInput />
+                <Flex sx={{ alignItems: 'center' }}>
+                  <NavLink href="https://github.com/system-ui/theme-ui">
+                    GitHub
+                  </NavLink>
+                  <Button
+                    aria-label={`Change color mode to ${nextColorMode}`}
+                    sx={{
+                      ml: 2,
+                      whiteSpace: 'pre',
+                    }}
+                    onClick={() => setMode(nextColorMode)}
+                  >
+                    {getModeName(mode)}
+                  </Button>
+                </Flex>
+              </Flex>
+            </Suspense>
+          </Flex>
+        )}
+        <Box
+  
