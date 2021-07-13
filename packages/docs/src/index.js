b485f15d87789b7cc5e29f182dd6bbb4824207e3
eightmen <118704527+eightmen@users.noreply.github.com>
2021-07-13 12:54:38 +0000

    Release v0.1.5

--- packages/docs/src/index.js
+++ packages/docs/src/index.js
@@ -48,4 +48,33 @@ export const setDocSearchComponents = ({
             var searchSelector = "#algolia-docs-search";
             var searchInput = document.querySelector(searchSelector);
 
-            if (searchIn
+            if (searchInput && (lastSearchInput !== searchInput)) {
+              docsearch({
+                apiKey: "84ed820927eee5fa5018c9f1abe70390",
+                indexName: "theme-ui",
+                inputSelector: searchSelector,
+                debug: true,
+                transformData: function(hits) {
+                  for (const hit of hits) {
+                    hit.url = hit.url.replace(
+                      "https://theme-ui.com",
+                      window.location.origin
+                    )
+                  }
+                }
+              })
+              lastSearchInput = searchInput;
+            }
+          });
+
+          document.addEventListener("DOMContentLoaded", function() {
+            observer.observe(document, {
+              childList: true,
+              subtree: true
+            });
+          });        
+      `,
+      }}
+    />,
+  ])
+}
