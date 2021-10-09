2e517e478ed0247212f0c96635656e5d602a7d8a
eightmen <118704527+eightmen@users.noreply.github.com>
2021-10-09 10:19:01 +0000

    ups

--- packages/docs/src/pages/recipes/index.js
+++ packages/docs/src/pages/recipes/index.js
@@ -25,4 +25,28 @@ const RecipeListItem = ({ id, pageContext, path }) => {
 
   return (
     <Themed.li>
-      {/* TODO: Cards with rende
+      {/* TODO: Cards with rendered recipes would be cool, but we'll need
+          some tests for them first.
+          Check out commit 67e112cb547f93e6b30a09d03e218e335742be76 for preview
+          card implementation.  */}
+      <Link to={path}>{name}</Link>
+    </Themed.li>
+  )
+}
+
+export default function RecipesPage(props) {
+  const recipes = props.data.allSitePage.nodes
+
+  return (
+    <div sx={{}}>
+      <Themed.h1>Recipes</Themed.h1>
+      <Cards columns={2}>
+        <Themed.ul>
+          {recipes.map((recipe) => (
+            <RecipeListItem key={recipe.id} {...recipe} />
+          ))}
+        </Themed.ul>
+      </Cards>
+    </div>
+  )
+}
