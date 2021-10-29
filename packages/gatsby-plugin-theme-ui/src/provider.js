2a93189e8b5de98474d55471a665cb4d66d1abc1
eightmen <118704527+eightmen@users.noreply.github.com>
2021-10-29 18:13:27 +0000

    delete unused files

--- packages/gatsby-plugin-theme-ui/src/provider.js
+++ packages/gatsby-plugin-theme-ui/src/provider.js
@@ -1,1 +1,38 @@
-import { ThemeProvider,
+import { ThemeProvider, merge } from 'theme-ui'
+import { useThemedStylesWithMdx } from '@theme-ui/mdx'
+import React from 'react'
+
+import localTheme from './index'
+import components from './components'
+import useThemeUiConfig from './hooks/configOptions'
+
+import { MDXProvider, useMDXComponents } from '@mdx-js/react'
+
+const Root = ({ children }) => {
+  const themeUiConfig = useThemeUiConfig()
+  const { preset, prismPreset } = themeUiConfig
+
+  const theme = preset.default || preset
+
+  const themeWithPrism = merge(theme, {
+    styles: {
+      pre: prismPreset,
+    },
+  })
+
+  const fullTheme = merge(themeWithPrism, localTheme)
+
+  return (
+    <ThemeProvider theme={fullTheme}>
+      <MDXProvider
+        components={useThemedStylesWithMdx(useMDXComponents(components))}
+      >
+        {children}
+      </MDXProvider>
+    </ThemeProvider>
+  )
+}
+
+export const WrapRootElement = ({ element }) => {
+  return <Root>{element}</Root>
+}
---
+++ packages/gatsby-plugin-theme-ui/test/__mocks__/gatsby.js
@@ -0,0 +1,12 @@
+const gatsby = jest.requireActual('gatsby')
+
+module.exports = {
+  ...gatsby,
+  graphql: jest.fn(),
+  useStaticQuery: jest.fn(() => ({
+    themeUiConfig: {
+      preset: {},
+      prismPreset: {},
+    },
+  })),
+}
