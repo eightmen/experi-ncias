4c31d6b64c59ef38ff171c6d256559966ae8e584
eightmen <118704527+eightmen@users.noreply.github.com>
2021-10-29 09:17:58 +0000

    vbump

--- packages/e2e/support.ts
+++ packages/e2e/support.ts
@@ -4,4 +4,33 @@ import * as toolbelt from 'ts-toolbelt'
 // Workaround for @testing-library/dom problem.
 // We should be able remove it in the future.
 if (typeof global === 'undefined') {
-  Object.assign(window, { global
+  Object.assign(window, { global: window })
+}
+
+require('@testing-library/cypress/add-commands')
+require('@percy/cypress')
+
+declare global {
+  namespace Cypress {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interface Chainable<Subject> extends Chainables<typeof commands> {}
+  }
+}
+
+export {}
+
+const commands = {}
+
+type Chainables<T extends object> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => any
+    ? (
+        ...args: toolbelt.List.Tail<Parameters<T[K]>>
+      ) => Cypress.Chainable<ReturnType<T[K]>>
+    : never
+}
+
+Cypress.on('uncaught:exception', (_err, _runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false
+})
