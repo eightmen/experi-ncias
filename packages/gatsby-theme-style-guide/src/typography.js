2e419f205cfae01f9e893fd27cf73dc5e8000dc2
eightmen <118704527+eightmen@users.noreply.github.com>
2021-11-08 13:10:02 +0000

    merge parent

--- packages/gatsby-theme-style-guide/src/typography.js
+++ packages/gatsby-theme-style-guide/src/typography.js
@@ -1,4 +1,77 @@
 // @ts-check
 import React from 'react'
 import { Flex, useThemeUI } from 'theme-ui'
-import { Themed } f
+import { Themed } from '@theme-ui/mdx'
+import { TypeScale, TypeStyle } from '@theme-ui/style-guide'
+
+const Row = (props) => (
+  <Flex
+    {...props}
+    sx={{
+      alignItems: 'baseline',
+      flexWrap: 'wrap',
+      mx: -3,
+      '& > div': {
+        px: 3,
+      },
+    }}
+  />
+)
+
+export default function Typography() {
+  const { theme } = useThemeUI()
+  const { fonts, fontWeights, lineHeights } = theme
+
+  return (
+    <section id="typography">
+      <Themed.h2>Typography</Themed.h2>
+      {fonts && (
+        <div>
+          <Themed.h3>Font Families</Themed.h3>
+          <Row>
+            {Object.keys(fonts).map((name) => (
+              <div key={name}>
+                <TypeStyle fontFamily={name} fontSize={6}>
+                  Aa
+                </TypeStyle>
+                <Themed.code title={fonts[name]}>{name}</Themed.code>
+              </div>
+            ))}
+          </Row>
+        </div>
+      )}
+      <Themed.h3>Font Sizes</Themed.h3>
+      <TypeScale />
+      {fontWeights && (
+        <div>
+          <Themed.h3>Font Weights</Themed.h3>
+          <Row>
+            {Object.keys(fontWeights).map((name) => (
+              <div key={name}>
+                <TypeStyle fontSize={6} fontWeight={name}>
+                  {fontWeights[name]}
+                </TypeStyle>
+                <Themed.code>{name}</Themed.code>
+              </div>
+            ))}
+          </Row>
+        </div>
+      )}
+      {lineHeights && (
+        <div>
+          <Themed.h3>Line Heights</Themed.h3>
+          <Row>
+            {Object.keys(lineHeights).map((name) => (
+              <div key={name}>
+                <TypeStyle fontSize={6} lineHeight={name}>
+                  {lineHeights[name]}
+                </TypeStyle>
+                <Themed.code>{name}</Themed.code>
+              </div>
+            ))}
+          </Row>
+        </div>
+      )}
+    </section>
+  )
+}
---
+++ packages/gatsby-theme-ui-layout/CHANGELOG.md
@@ -0,0 +1,12 @@
+# v0.12.0 (Thu Oct 28 2021)
+
+:tada: This release contains work from a new contributor! :tada:
+
+Thank you, William Pei ([@draekien](https://github.com/draekien)), for all your work!
+
+#### 🚀 Enhancement
+
+
+#### 🐛 Bug Fix
+
+- Merge remote
