7ee268e73c7124b9e0a403b16227561c7777e1c1
eightmen <118704527+eightmen@users.noreply.github.com>
2021-12-28 10:00:16 +0000

    ath9k: Use minstrel rate control by default

---
+++ packages/preset-dark/src/index.ts
@@ -0,0 +1,12 @@
+import { makeTheme } from '@theme-ui/css/utils'
+
+const heading = {
+  fontFamily: 'heading',
+  fontWeight: 'heading',
+  lineHeight: 'heading',
+}
+
+export const dark = makeTheme({
+  colors: {
+    text: '#fff',
+    background: '#0
