0dad7b86197d95c161b4d9107b18c5eca87ff49b
eightmen <118704527+eightmen@users.noreply.github.com>
2022-02-11 08:16:05 +0000

    removed unused folder

--- packages/preset-system/src/index.ts
+++ packages/preset-system/src/index.ts
@@ -54,4 +54,117 @@ export const system = makeTheme({
     },
   },
   fonts: {
-    body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Ne
+    body: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    heading: 'inherit',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    display: 900,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.25,
+  },
+  text: {
+    heading,
+    display: {
+      variant: 'text.heading',
+      fontSize: [5, 6],
+      fontWeight: 'display',
+      letterSpacing: '-0.03em',
+      mt: 3,
+    },
+  },
+  styles: {
+    Container: {
+      p: 3,
+      maxWidth: 1024,
+    },
+    root: {
+      fontFamily: 'body',
+      lineHeight: 'body',
+      fontWeight: 'body',
+    },
+    h1: {
+      variant: 'text.display',
+    },
+    h2: {
+      variant: 'text.heading',
+      fontSize: 5,
+    },
+    h3: {
+      variant: 'text.heading',
+      fontSize: 4,
+    },
+    h4: {
+      variant: 'text.heading',
+      fontSize: 3,
+    },
+    h5: {
+      variant: 'text.heading',
+      fontSize: 2,
+    },
+    h6: {
+      variant: 'text.heading',
+      fontSize: 1,
+    },
+    a: {
+      color: 'primary',
+      '&:hover': {
+        color: 'secondary',
+      },
+    },
+    pre: {
+      fontFamily: 'monospace',
+      fontSize: 1,
+      p: 3,
+      color: 'text',
+      bg: 'muted',
+      overflow: 'auto',
+      code: {
+        color: 'inherit',
+      },
+    },
+    code: {
+      fontFamily: 'monospace',
+      fontSize: 1,
+    },
+    inlineCode: {
+      fontFamily: 'monospace',
+      color: 'secondary',
+      bg: 'muted',
+    },
+    table: {
+      width: '100%',
+      my: 4,
+      borderCollapse: 'separate',
+      borderSpacing: 0,
+      'th,td': {
+        textAlign: 'left',
+        py: '4px',
+        pr: '4px',
+        pl: 0,
+        borderColor: 'muted',
+        borderBottomStyle: 'solid',
+      },
+    },
+    th: {
+      verticalAlign: 'bottom',
+      borderBottomWidth: '2px',
+    },
+    td: {
+      verticalAlign: 'top',
+      borderBottomWidth: '1px',
+    },
+    hr: {
+      border: 0,
+      borderBottom: '1px solid',
+      borderColor: 'muted',
+    },
+  } as const,
+})
+
+export default system
---
+++ packages/preset-tailwind/README.md
@@ -0,0 +1,1 @@
+# [@theme-ui/preset-ta
