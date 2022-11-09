5637933e677febc08da1a7bbd8ad176eb4b69978
eightmen <118704527+eightmen@users.noreply.github.com>
2022-11-09 09:13:23 +0000

    model

---
+++ packages/typography/test/styles.ts
@@ -0,0 +1,83 @@
+
+import { styles } from '../src'
+
+test('snapshot', () => {
+  expect(styles).toMatchInlineSnapshot(`
+    {
+      "b": {
+        "fontWeight": "bold",
+      },
+      "blockquote": {
+        "margin": 0,
+        "marginBottom": 3,
+        "mx": 3,
+        "padding": 0,
+      },
+      "code": {
+        "fontSize": "85%",
+      },
+      "h1": {
+        "fontFamily": "heading",
+        "fontSize": 5,
+        "fontWeight": "heading",
+        "lineHeight": "heading",
+        "margin": 0,
+        "marginBottom": 3,
+        "padding": 0,
+      },
+      "h2": {
+        "fontFamily": "heading",
+        "fontSize": 4,
+        "fontWeight": "heading",
+        "lineHeight": "heading",
+        "margin": 0,
+        "marginBottom": 3,
+        "padding": 0,
+      },
+      "h3": {
+        "fontFamily": "heading",
+        "fontSize": 3,
+        "fontWeight": "heading",
+        "lineHeight": "heading",
+        "margin": 0,
+        "marginBottom": 3,
+        "padding": 0,
+      },
+      "h4": {
+        "fontFamily": "heading",
+        "fontSize": 2,
+        "fontWeight": "heading",
+        "lineHeight": "heading",
+        "margin": 0,
+        "marginBottom": 3,
+        "padding": 0,
+      },
+      "h5": {
+        "fontFamily": "heading",
+        "fontSize": 1,
+        "fontWeight": "heading",
+        "lineHeight": "heading",
+        "margin": 0,
+        "marginBottom": 3,
+        "padding": 0,
+      },
+      "h6": {
+        "fontFamily": "heading",
+        "fontSize": 0,
+        "fontWeight": "heading",
+        "lineHeight": "heading",
+        "margin": 0,
+        "marginBottom": 3,
+        "padding": 0,
+      },
+      "hr": {
+        "border": 0,
+        "borderBottom": "1px solid",
+        "margin": 0,
+        "marginBottom": 3,
+        "mb": 3,
+        "mt": "-1px",
+        "padding": 0,
+      },
+      "img": {
+        "margin": 0,
